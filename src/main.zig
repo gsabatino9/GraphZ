982c8816d311855e8235a388b870e8d6f0e993c8
gsabatino9 <gsabatino@fi.uba.ar>
2024-05-16 20:43:27 -0300

    agrego structs

--- src/main.zig
+++ src/main.zig
@@ -4,6 +4,7 @@ const Allocator = std.mem.Allocator;
 const stdin = std.io.getStdIn().reader();
 
 const ReadError = error{BadRead};
+const GraphError = error{ PutError, RelationError };
 
 pub fn get_key_value(allocator: Allocator) ![2][]u8 {
     var buf: [30]u8 = undefined;
@@ -37,56 +38,88 @@ pub fn get_key_value(allocator: Allocator) ![2][]u8 {
     return arr;
 }
 
-pub fn imp_hash(allocator: Allocator) !void {
-    var map = std.StringHashMap(std.ArrayList([]u8)).init(allocator);
-    defer {
-        var it = map.iterator();
+const Graph = struct {
+    map: std.StringHashMap(std.ArrayList([]u8)),
+    allocator: Allocator,
+
+    pub fn init(allocator: Allocator) Graph {
+        return .{ .map = std.StringHashMap(std.ArrayList([]u8)).init(allocator), .allocator = allocator };
+    }
+
+    pub fn deinit(self: *Graph) void {
+        var it = self.map.iterator();
         while (it.next()) |entry| {
             const key = entry.key_ptr.*;
-            allocator.free(key);
+            self.allocator.free(key);
             const adjacencies = entry.value_ptr.*;
             for (adjacencies.items) |adj| {
-                allocator.free(adj);
+                self.allocator.free(adj);
             }
             adjacencies.deinit();
         }
-        map.deinit();
+
+        self.map.deinit();
     }
+
+    pub fn add_relation(self: *Graph, key: []u8, value: []u8) !bool {
+        const v = self.map.getOrPut(key) catch {
+            return GraphError.PutError;
+        };
+
+        if (!v.found_existing) {
+            var adjacencies = std.ArrayList([]u8).init(self.allocator);
+            try adjacencies.append(value);
+            v.value_ptr.* = adjacencies;
+            return false;
+        } else {
+            try v.value_ptr.*.append(value);
+            return true;
+        }
+    }
+
+    pub fn add_relation_release_memory(self: *Graph, key: []u8, value: []u8) !void {
+        const key_exists = self.add_relation(key, value) catch {
+            self.allocator.free(key);
+            self.allocator.free(value);
+            return GraphError.RelationError;
+        };
+        if (key_exists) {
+            self.allocator.free(key);
+        }
+    }
+
+    pub fn print_relations(self: *Graph) void {
+        var it = self.map.iterator();
+        while (it.next()) |entry| {
+            const key = entry.key_ptr.*;
+            const adjs = entry.value_ptr.*;
+
+            print("{s} -> [", .{key});
+            for (adjs.items) |item| {
+                print("{s}, ", .{item});
+            }
+            print("]\n", .{});
+        }
+    }
+};
+
+pub fn imp_hash(allocator: Allocator) !void {
+    var graph = Graph.init(allocator);
+    defer graph.deinit();
 
     while (true) {
         const key_value = get_key_value(allocator) catch {
             break;
         };
         const key = key_value[0];
-        const val = key_value[1];
+        const value = key_value[1];
 
-        const v = map.getOrPut(key) catch {
-            allocator.free(key);
-            allocator.free(val);
+        graph.add_relation_release_memory(key, value) catch {
             break;
         };
-
-        if (!v.found_existing) {
-            var adjacencies = std.ArrayList([]u8).init(allocator);
-            try adjacencies.append(val);
-            v.value_ptr.* = adjacencies;
-        } else {
-            try v.value_ptr.*.append(val);
-            allocator.free(key);
-        }
     }
 
-    var it = map.iterator();
-    while (it.next()) |entry| {
-        const key = entry.key_ptr.*;
-        const adjs = entry.value_ptr.*;
-
-        print("{s} -> [", .{key});
-        for (adjs.items) |item| {
-            print("{s}, ", .{item});
-        }
-        print("]\n", .{});
-    }
+    graph.print_relations();
 }
 
 pub fn main() !void {
